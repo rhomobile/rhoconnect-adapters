312e3df182a075b068c956cbe9b2f895a8920339
Maxim Zverev <max@rhomobile.com>
2011-06-22 11:18:32 -0700

    Initial updates

--- generators/rhocrm.rb
+++ generators/rhocrm.rb
@@ -1,5 +1,9 @@
 require 'rubygems'
 require 'rhosync'
+
+#puts File.dirname('rhosync')
+
+#require File.join('rhosync', '..','..','generators','rhosync')
 require 'templater'
 
 module Rhocrm
@@ -10,36 +14,58 @@ module Rhocrm
     Rhocrm generator
   DESC
   
-#  class SourceGenerator < BaseGenerator
-#    def self.source_root
-#      File.join(File.dirname(__FILE__), 'templates', 'source')
-#    end
+  class BaseGenerator < Templater::Generator
+    def class_name
+      name.gsub('-', '_').camel_case
+    end
+    
+    def underscore_name
+      Rhosync.under_score(name)
+    end
+    
+    def crm_backend
+      crm
+    end
 
-#    desc <<-DESC
-#      Generates a new source adapter.
+    def gem_version
+      VERSION
+    end 
+    
+    alias_method :module_name, :class_name
+  end
+  
+  class SourceGenerator < BaseGenerator
+    def self.source_root
+      File.join(File.dirname(__FILE__), 'templates', 'source')
+    end
+
+    desc <<-DESC
+      Generates a new source adapter based on CRM object.
       
-#      Required:
-#        name        - source name(i.e. product)
-#    DESC
+      Required:
+        <name>        - source name(i.e. Account)
+        <CRM base>    - name of the CRM backend
+    DESC
 
-#    first_argument :name, :required => true, :desc => "source name"
+    first_argument :name, :required => true, :desc => "source name"
+    second_argument :crm, :required => true, :desc => "CRM backend name"
 
-#    template :source do |template|
-#      template.source = 'source_adapter.rb'
-#      template.destination = "sources/#{underscore_name}.rb"
-#      settings_file = File.join(@destination_root,'settings','settings.yml')
-#      settings = YAML.load_file(settings_file)
-#      settings[:sources] ||= {}
-#      settings[:sources][class_name] = {:poll_interval => 300}
-#      File.open(settings_file, 'w' ) do |file|
-#        file.write "#Sources" + {:sources => settings[:sources]}.to_yaml[3..-1]
-#        envs = {}
-#        [:development,:test,:production].each do |env|
-#          envs[env] = settings[env]
-#        end
-#        file.write envs.to_yaml[3..-1]
-#      end
-#    end
+    template :source do |template|
+      template.source = 'source_adapter.rb'
+      template.destination = "sources/#{underscore_name}.rb"
+      settings_file = File.join(@destination_root,'settings','settings.yml')
+      settings = YAML.load_file(settings_file)
+      settings[:sources] ||= {}
+      settings[:sources][class_name] = {:poll_interval => 300}
+      File.open(settings_file, 'w' ) do |file|
+        file.write "#Sources" + {:sources => settings[:sources]}.to_yaml[3..-1]
+        envs = {}
+        [:development,:test,:production].each do |env|
+          envs[env] = settings[env]
+        end
+        file.write envs.to_yaml[3..-1]
+      end
+    end
     
 #    template :source_spec do |template|
 #      template.source = 'source_spec.rb'
@@ -48,5 +74,5 @@ module Rhocrm
 #  end
   
 #  add :app, AppGenerator
-  add :source, Rhosync::SourceGenerator
+  add :source, SourceGenerator
 end
---
+++ generators/templates/source/source_adapter.rb
@@ -0,0 +1,40 @@
+require '<%=crm_backend%>'
+
+class <%=class_name%> < Rhocrm::<%=crm_backend%>::Adapter
+  def initialize(source,credential)
+    super(source, credential)
+  end
+ 
+  def login
+    super
+    # initialize fields map
+  end
+ 
+  def query(params=nil)
+    super(params)
+  end
+  
+  def metadata
+    super
+  end
+ 
+  def sync
+    super
+  end
+ 
+  def create(create_hash,blob=nil)
+    super(create_hash,blob)
+  end
+ 
+  def update(update_hash)
+    super(update_hash)
+  end
+ 
+  def delete(delete_hash)
+    super(delete_hash)
+  end
+ 
+  def logoff
+    super
+  end
+end
